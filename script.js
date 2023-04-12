d23ace17d4723d460dfc3a9ba3b71eb5af864b86
Drew S <andrewsantos2022@u.northwestern.edu>
2023-04-11 23:50:00 -0500

    bug fixes

--- script.js
+++ script.js
@@ -1,137 +1,140 @@
-// global variables
-let timer = document.querySelector("#timer");
-let timeLeft = 60;
-let time = 0;
-let currentScore = 0;
-let start = document.querySelector('#startBtn');
-let score = JSON.parse(localStorage.getItem("score")) || [];
-let questions = document.querySelector("#quiz");
-let questionIndex = 0;
+let time = 30;
+let timer = 0;
+let score = 0;
+let startQuizBtn = document.querySelector(`#startQuiz`);
+let questionDiv = document.querySelector(`#questions`);
+let timerDiv = document.querySelector(`#timer`);
+let intro = document.querySelector("#intro");
+let userChoice = "";
+let scores = JSON.parse(localStorage.getItem("score")) || [];
+let questionsIndex = 0;
 let quiz = [
-    {
-    question: "Which of the following do you loop through in Javascript?",
-    answers: ["object", "function", "array", "element"],
-    correctAnswer: "array",
+  {
+    title: "Which of the following do you loop through in Javascript?",
+    choices: ["object", "function", "array", "element"],
+    answer: "array",
+  },
+  { 
+    title: "What data type is this value? value: 500", 
+    choices: ["boolean", "string", "undefined", "integer"], 
+    answer: "integer" 
 },
-{
-    question: "What data type is this value? value: 500",
-    answers: ["boolean", "string", "undefined", "integer"],
-    correctAnswer: "integer",
+  { 
+    title: "What is the correct syntax for displaying data in the console?", 
+    choices: ["console.log()", "console.log{}", "log.console()", "consolelog()"], 
+    answer: "console.log()" 
 },
-{
-    question: "What is the correct syntax for displaying data in the console?",
-    answers: ["console.log()", "console.log{}", "log.console()", "consolelog()"],
-    correctAnswer: "console.log()",   
-},
-{
-    question: "Which of the following defines an object in javascript?",
-    answers: ["An object is a variable.", "An object is a container for named values called properties.", "An object is a function.", "An object is a fruit."],
-    correctAnswer: "An object is a container for named values called properties."
-},
+  { 
+    title: "Which of the following defines an object in javascript?", 
+    choices: ["An object is a variable.", "An object is a container for named values called properties.", "An object is a function.", "An object is a fruit."],
+    answer: "An object is a container for named values called properties." 
+} 
 ];
 
 
+
 // functions
-// first question
-function firstQuestion() {
-let question = document.createElement("h2");
-question.textContent = quiz[questionIndex].question;
-questions.appendChild(question);
-
-// create buttons and append to questions div
-let btnOne = document.createElement("button");
-btnOne.textContent = quiz[questionIndex].answers[0];
-questions.appendChild(btnOne);
-
-let btnTwo = document.createElement("button");
-btnTwo.textContent = quiz[questionIndex].answers[1];
-questions.appendChild(btnTwo);
-
-let btnThree = document.createElement("button");
-btnThree.textContent = quiz[questionIndex].answers[2];
-questions.appendChild(btnThree);
-
-let btnFour = document.createElement("button")
-btnFour.textContent = quiz[questionIndex].answers[3];
-questions.appendChild(btnFour);
-};
-
-function clearPrevious() {};
-
-function nextQuestion() {
-
-}
-// function to compare user answer to correct answer
-
-function userChoice() {
-    if (this.textContent === quiz[questionIndex].correctAnswer) {
-        questionIndex++;
-        score++;
-    if (questionIndex === quiz.length) {
-        alert("You fail.");
-        clearInterval(time); 
-    }   else {
-        let incorrect = document.createElement(h3);
-        incorrect.textContent = "Wrong Answer."
-        questions.appendChild(incorrect)
-        questions.innerHTML = '';
-        nextQuestion();
-    }
-
-    }
-}
-
-// timer function
-function startBtn() {
-    questionIndex = 0;
-    questions.innerHTML = "";
-    time = setInterval(function() {
-        timeLeft--, timer.innerHTML = timeLeft;
+function startQuiz() {
+    timer = setInterval(function () {
+        time--;
+        timerDiv.textContent = time;
+        if (time <= 0) {
+            clearInterval(timer);
+            endGame();
+        }
     }, 1000);
-};
+    intro.classList.add("hidden");
+    createButton(questionsIndex);
+}
+  
 
-// function to save score to local storage
-function saveScore() {
-    localStorage.setItem("score", JSON.stringify(score));
+// create buttons for each question
+function createButton(index) {
+    questionDiv.innerHTML = "";
+    let questionTitle = document.createElement("h2");
+    questionTitle.textContent = quiz[index].title;
+    questionDiv.appendChild(questionTitle);
+    let questionChoices = quiz[index].choices;
+    questionChoices.forEach((choice) => {
+        let questionBtn = document.createElement("button");
+        questionBtn.textContent = choice;
+        questionBtn.classList = "btn";
+        questionBtn.dataset.answer = quiz[index].answer;
+        questionDiv.appendChild(questionBtn);
+    });
 }
 
-// function to display high scores
-function highScore() {
-    let highScore = document.createElement("h2");
-    highScore.textContent = "High Scores";
-    questions.appendChild(highScore);
-    let scoreList = document.createElement("ol");
-    questions.appendChild(scoreList);
-    for (let i = 0; i < score.length; i++) {
-        let scoreItem = document.createElement("li");
-        scoreItem.textContent = score[i];
-        scoreList.appendChild(scoreItem);
-    }
+function endGame() {
+    questionDiv.innerHTML = "";
+    let endGameDiv = document.createElement("div");
+    endGameDiv.classList = "endGame";
+    let endGameTitle = document.createElement("h2");
+    endGameTitle.textContent = "All done!";
+    endGameDiv.appendChild(endGameTitle);
+    let endGameP = document.createElement("p");
+    endGameP.textContent = `Your final score is ${time}`;
+    endGameDiv.appendChild(endGameP);
+    let endGameForm = document.createElement("form");
+    endGameForm.id = "initials";
+    let endGameLabel = document.createElement("label");
+    endGameLabel.textContent = "Enter initials: ";
+    endGameForm.appendChild(endGameLabel);
+    let endGameInput = document.createElement("input");
+    endGameInput.id = "initialsInput";
+    endGameForm.appendChild(endGameInput);
+    let endGameBtn = document.createElement("button");
+    endGameBtn.textContent = "Submit";
+    endGameBtn.id = "submitBtn";
+    endGameForm.appendChild(endGameBtn);
+    endGameDiv.appendChild(endGameForm);
+    questionDiv.appendChild(endGameDiv);
+    clearInterval(timer);
+    endGameBtn.addEventListener("click", saveScore);
 }
 
-// function to clear high scores
-function clearScore() {
-    localStorage.clear();
-    questions.innerHTML = "";
+// check answer function
+function checkAnswer(event) {
+  let userAnswer = event.target.dataset.answer;
+  let userChoice = event.target.textContent;
+  if (userAnswer === userChoice) {
+    alert("Correct!");
+  } else {
+    alert("Incorrect!");
+    time -= 10;
+  }
+  questionsIndex++;
+  if (questionsIndex < quiz.length) {
+    createButton(questionsIndex);
+  } else {
+    endGame();
+  }
 }
 
-// function to restart quiz
-function restart() {
-    questions.innerHTML = "";
-    timeLeft = 60;
-    timer.innerHTML = timeLeft;
-    questionIndex = 0;
-    score = 0;
-    startBtn();
-    firstQuestion();
+function saveScore(event) {
+  event.preventDefault();
+  let initials = document.querySelector("#initialsInput").value;
+  let score = {
+    initials: initials,
+    score: time,
+  };
+  scores.push(score);
+  localStorage.setItem("score", JSON.stringify(scores));
+  window.location.href = "highscores.html";
 }
+
 
 // event listeners
-start.addEventListener("click", startBtn)
-btnOne.addEventListener("click", userChoice); 
-btnTwo.addEventListener("click", userChoice);
-btnThree.addEventListener("click", userChoice);
-btnFour.addEventListener("click", userChoice);
+startQuizBtn.addEventListener("click", startQuiz);
+questionDiv.addEventListener("click", checkAnswer);
+
+
+
+
+
+
+
+    
+
 
 
 
